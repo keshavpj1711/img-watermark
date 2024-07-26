c2117bb7a9801df294aaaa2ba53b0faab80528eb
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-26 14:24:57 +0530

    fixed ui and added a load button to load a new image into the editor

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 from image_processing import ImageProcessing
 
 
@@ -22,14 +23,14 @@ class UserInterface:
     self.left_frame = Frame(self.root, width=400, height=780, bg='white')
     self.left_frame.grid(row=0, column=0, padx=10, pady=10)
 
-    self.right_frame = Frame(self.root, width=1160, height=780, bg='white')
+    self.right_frame = Frame(self.root, width=1150, height=780, bg='white')
     self.right_frame.grid(row=0, column=1, padx=10, pady=10)
 
     # Left frame
     self.resized_image = ImageProcessing.resize_image(self.img_path, 400, 225)
     Label(self.left_frame, image=self.resized_image).grid(row=0, column=0, padx=5, pady=5, columnspan=2)
 
-    load_button = Button(self.left_frame, text="Load", width=20)
+    load_button = Button(self.left_frame, text="Load", width=20, command=self.load_path)
     load_button.grid(row=1, column=0, columnspan=2)
 
     # Adding a text box
@@ -41,9 +42,15 @@ class UserInterface:
         "Arial", 12, "normal"), fg='black')
     self.text_entry.grid(row=2, column=1, pady=5)
 
-  def load_path():
+  def load_path(self):
     # This will give a dialog box to select the image from the menu
-    pass
+    self.img_path = filedialog.askopenfilename(
+        initialdir="./", 
+        title="Select file", 
+        filetypes=(("all files", "*.*"), ("Image files", "*.jpg;*.jpeg;*.png;*.bmp"))
+    )
+
+    self.setup_gui()
 
   def run(self):
     self.root.mainloop()
